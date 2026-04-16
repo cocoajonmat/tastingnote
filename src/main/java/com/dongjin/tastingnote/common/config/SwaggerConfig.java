1e35255bc04f5b7f1baadea4d2ea782e269d9358
하동진 <gkehdwls123@naver.com>
2026-04-16 10:04:11 +0900

    fix: Swagger 서버 URL 환경변수로 관리 (Mixed Content 해결)

--- src/main/java/com/dongjin/tastingnote/common/config/SwaggerConfig.java
+++ src/main/java/com/dongjin/tastingnote/common/config/SwaggerConfig.java
@@ -4,15 +4,22 @@ import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
 
+    @Value("${swagger.server-url:}")
+    private String serverUrl;
+
     @Bean
     public OpenAPI openAPI() {
-        return new OpenAPI()
+        OpenAPI openAPI = new OpenAPI()
                 .info(new Info()
                         .title("TastingNote API")
                         .description("술 테이스팅 노트 서비스 API")
@@ -22,5 +29,11 @@ public class SwaggerConfig {
                                 .type(SecurityScheme.Type.HTTP)
                                 .scheme("bearer")
                                 .bearerFormat("JWT")));
+
+        if (!serverUrl.isBlank()) {
+            openAPI.servers(List.of(new Server().url(serverUrl)));
+        }
+
+        return openAPI;
     }
 }
